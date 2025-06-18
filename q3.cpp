57fb80b579f33e191c88470621825f12b7d87e8d
sh-gun14 <shagunhudda1409@gmail.com>
2025-06-19 03:47:10 +0530

    Update q3.cpp

--- q3.cpp
+++ q3.cpp
@@ -1,42 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int findPeak(vector<int>& a) {
-    int l = 0, r = a.size() - 1;
-    while (l < r) {
-        int mid = (l + r) / 2;
-        if (a[mid] < a[mid + 1]) l = mid + 1;
-        else r = mid;
+#include <bits/stdc++.h> 
+bool checkPalindrome(string s)
+{
+    // Write your code here.
+    string resp;
+    for(auto ch:s)
+    {
+if(isalnum(ch))
+        resp+=tolower(ch);
     }
-    return l;
-}
+stack<int>st;
 
-int binarySearch(vector<int>& a, int l, int r, int x, bool asc) {
-    while (l <= r) {
-        int mid = (l + r) / 2;
-        if (a[mid] == x) return mid;
-        if (asc) {
-            if (a[mid] < x) l = mid + 1;
-            else r = mid - 1;
-        } else {
-            if (a[mid] > x) l = mid + 1;
-            else r = mid - 1;
-        }
+    for(auto val:resp)
+
+    st.push(val);
+    for (char ch : resp) {
+            if (st.top() != ch ){
+                return false;  
+            }
+  st.pop();
     }
-    return -1;
-}
 
-int bitonicSearch(vector<int>& a, int x) {
-    int peak = findPeak(a);
-    int left = binarySearch(a, 0, peak, x, true);
-    if (left != -1) return left;
-    return binarySearch(a, peak + 1, a.size() - 1, x, false);
-}
-
-int main() {
-    int n, x; cin >> n >> x;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    cout << bitonicSearch(a, x) << endl;
-    return 0;
+    return true;
 }
