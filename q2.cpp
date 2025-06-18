7ab2a61595fa63a046764f5e32396958bfefaff4
sh-gun14 <shagunhudda1409@gmail.com>
2025-06-19 03:45:47 +0530

    Update q2.cpp

--- q2.cpp
+++ q2.cpp
@@ -1,20 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int findMin(vector<int>& a) {
-    int l = 0, r = a.size() - 1;
-    while (l < r) {
-        int mid = (l + r) / 2;
-        if (a[mid] > a[r]) l = mid + 1;
-        else r = mid;
-    }
-    return a[l];
-}
-
-int main() {
-    int n; cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) cin >> a[i];
-    cout << findMin(a) << endl;
-    return 0;
+void reverseArray(vector<int> &arr, int m) {
+    int n = arr.size();
+    reverse(arr.begin() + m + 1, arr.end());
 }
